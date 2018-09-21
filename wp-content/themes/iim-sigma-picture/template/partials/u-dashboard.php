904df5510fb7d3baa82cfd8dd2b1f1742146f0fe
Colombe Chevance <chevanccolombe@gmail.com>
2018-09-21 22:01:03 +0200

    fin travail

--- wp-content/themes/iim-sigma-picture/template/partials/u-dashboard.php
+++ wp-content/themes/iim-sigma-picture/template/partials/u-dashboard.php
@@ -2,18 +2,22 @@
 	<div class="row">
 		<div class="col-md-6">
 			<?php
+            $user = wp_get_current_user();
+            $email = $user->user_email;
+
 			$args = [
 				'post_type'      => 'files',
-				'posts_per_page' => 10,
+				'posts_per_page' => -1,
+                's' => $email,
 			];
 
 			$lastFiles = new WP_Query( $args );
 			?>
+
 			<h4>Fichiers reçus </h4>
 			<table class="table">
                 <thead class="thead-dark">
 				<tr>
-					<th scope="col">Email</th>
 					<th scope="col">Fichier</th>
 				</tr>
 				</thead>
@@ -74,17 +78,14 @@
             <h4> Envoyer un fichier </h4>
             <form action="<?php echo get_template_directory_uri(); ?>/function/front/endpoint.php" id="qq-form" class="form-wrap">
                 <div class="form-group">
-                    <?php
-                    $user = wp_get_current_user();
-                    $email = $user->user_email;
-                    ?>
+
                     <input for="user_email" type="email"  placeholder="Email" class="form-control" id="user_email" name="user_email" hidden value="<?= $email; ?>">
                 </div>
                 <div class="form-group">
                     <div id="fine-uploader-manual-trigger"></div>
                 </div>
                 <div class="form-group">
-                    <input type="submit" value="Créer le compte" class="btn btn-primary btn-upload">
+                    <input type="submit" value="Envoyer le fichier" class="btn btn-primary btn-upload">
                 </div>
             </form>
         </div>
